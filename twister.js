08f6fafd64b6d8f8a4232193e01a36594e699c1c
sasha <sasha.friis@icloud.com>
2025-08-06 18:03:21 +0200

    Get twisted

---
+++ twister.js
@@ -0,0 +1,67 @@
+const canvas = document.getElementById('wheel');
+const wheel = canvas.getContext('2d');
+const result = document.getElementById('result');
+
+const segments = [];
+const colors = ['Red', 'Blue', 'Green', 'Yellow'];
+const limbs = ['Left Hand', 'Right Hand', 'Left Foot', 'Right Foot'];
+
+// Create all 16 combinations
+limbs.forEach(limb => {
+    colors.forEach(color => {
+        // ammend new elements to end of array
+        segments.push(`${limb} - ${color}`)
+    });
+});
+
+const segmentAngle = (2 * Math.PI) / segments.length;
+
+function drawWheel() {
+    for (let i = 0; i < segments.length; i++) {
+        const angle = i * segmentAngle;
+
+        wheel.beginPath();
+        wheel.moveTo(250, 250);
+        wheel.arc(250, 250, 250, angle, angle + segmentAngle);
+        wheel.fillStyle = colors[i % colors.length];
+
+        wheel.fill();
+        wheel.stroke();
+
+        wheel.save();
+        wheel.translate(250, 250);
+        wheel.rotate(angle + segmentAngle / 2);
+
+        wheel.textAlign = 'center';
+        wheel.fillStyle = 'black';
+        wheel.font = '14px Mono';
+        wheel.fillText(segments[i], 160, 5);
+        wheel.restore();
+    }
+}
+
+drawWheel()
+
+document.getElementById('spin').addEventListener('click', () => {
+    const spins = Math.floor(Math.round(4.5));
+    const randomSegment = Math.floor(Math.random() * segments.length);
+    const rotation = (360 / segments.length) * randomSegment + spins * 360;
+
+    canvas.style.transition = 'transform 3s ease-out';
+    canvas.style.transform = `rotate(${rotation}deg)`;
+    setTimeout(() => {
+        const resultText = segments[randomSegment];
+        const color = resultText.split(' - ')[1];
+        result.textContent = segments[randomSegment];
+        result.style.color = color.toLowerCase();
+        result.style.display = 'unset';
+        canvas.style.transition = 'none';
+        canvas.style.transform = 'rotate(0deg)';
+        result.fill();
+        result.stroke();
+        result.save();
+
+        drawWheel();
+
+    }, 3200);
+})
